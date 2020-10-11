31a1572733592af8973102730f3c1d34af090b9f
admin <admin@USER>
2020-10-12 00:45:58 +0800

    區塊1滑動視窗

---
+++ WebPage/block1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>jSelect</title>
+	<link href="css/block1.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+	<div class="slide-container" id="slide_component">
+		<ul class="slide-content">
+			<li><img src="images/pics03.jpg" width="200px" height="200px" /></li>
+			<li><img src="images/pics04.jpg" width="200px" height="200px" /></li>
+			<li><img src="images/pics06.jpg" width="200px" height="200px" /></li>
+		</ul>
+	</div>
+	<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+	<script type="text/javascript" src="js/banner.js"></script>
+	<script type="text/javascript">
+		$("#slide_component").jSlide();
+	</script>
+</body>
+</html>
