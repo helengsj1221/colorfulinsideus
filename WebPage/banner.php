1234c95338d91d550f75b6687b186c6ce2934d41
helen <helen@DESKTOP-IVS9STS>
2020-10-12 00:42:00 +0800

    首頁調整

--- WebPage/banner.php
+++ WebPage/banner.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
 <head>
 	<meta charset="utf-8">
-	<title>jSelect</title>
+	<title>banner</title>
 	<link href="css/banner.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
 	<div class="slide-container" id="slide_component">
 		<ul class="slide-content">
 			<li><img src="images/pics01.jpg" width="1280px" height="580px" /></li>
-			<li><img src="images/pics01.jpg" width="1280px" height="580px" /></li>
-			<li><img src="images/pics01.jpg" width="1280px" height="580px" /></li>
+			<li><img src="images/pics05.jpg" width="1280px" height="580px" /></li>
+			<li><img src="images/pics10.jpg" width="1280px" height="580px" /></li>
 		</ul>
-		<div class="slide-items-bg"></div>
 		<ul class="slide-items">
-			<li class="current-slide-item">1</li>
-			<li>2</li>
-			<li>3</li>
+			<li class="current-slide-item"></li>
+			<li></li>
+			<li></li>
 		</ul>
 	</div>
 	<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
