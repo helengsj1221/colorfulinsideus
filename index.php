2cd9fc0fa21f2c5ae95938916be746569e4acb69
helen <helen@DESKTOP-IVS9STS>
2020-10-15 02:33:09 +0800

    第一層目錄樣式測試套版

--- index.php
+++ index.php
@@ -21,10 +21,19 @@
 						<span>Inc.</span>
 					</div>
 					<nav id="nav">
-						<ul>
-							<li class="current_page_item"><a href="index.php">Homepage</a></li>
-							<li><a href="threecolumn.html">Other Column</a></li>
-
+						<ul class="drop-down-menu">
+							<li><a class="link link--index" href="index.php"><span data-letters-l="首" data-letters-r="頁">首頁</span></a></li>
+							<li><a class="link link--index" href="test.php"><span data-letters-l="公司" data-letters-r="簡介">公司簡介</span></a>
+								<ul>
+    								<li><a class="link link--secindex" href="test.php"><span data-letters="公司沿革">公司沿革</span></a></li><br>
+    								<li><a class="link link--secindex" href="test.php"><span data-letters="聯絡我們">聯絡我們</span></a></li><br>
+                            	</ul>
+							</li>
+							<li><a class="link link--index" href="test.php"><span data-letters-l="藝" data-letters-r="人">藝人</span></a></li>
+							<li><a class="link link--index" href="test.php"><span data-letters-l="影" data-letters-r="音">影音</span></a></li>
+							<li><a class="link link--index" href="test.php"><span data-letters-l="購物" data-letters-r="商城">購物商城</span></a></li>
+							<li><a class="link link--index" href="test.php"><span data-letters-l="會員" data-letters-r="專區">會員專區</span></a></li>
+							<li><a class="link link--index" href="test.php"><span data-letters-l="投" data-letters-r="票">投票</span></a></li>
 						</ul>
 					</nav>
 				</div>
