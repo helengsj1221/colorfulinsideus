b394b744ba198ed29cd4f3ffe896ff30397a260a
helen <helen@DESKTOP-IVS9STS>
2020-10-19 02:51:04 +0800

    新增目錄、新增最新消息、更正banner

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <html>
+	<?php 
+            // 連線至資料庫
+            $hostname  = "148.66.136.151:3306";
+            $username  = "Vicky";
+            $password  = "vicky@851221";
+            $dbname    = "ColorfulENTInc";
+            $today     = date("Ymd");  
+            $con = mysqli_connect($hostname,$username, $password,$dbname);
+            //撈資料
+            $newssql = "SELECT * FROM `BOARD` WHERE `BOARD_DATES`<= ".$today." AND `BOARD_DATEE` >=".$today;
+            mysqli_query($con, 'SET NAMES utf8');
+            $result = mysqli_query($con, $newssql);
+    ?>
 <div id="wrapper">
 	<head>
 		<title>Colorful Entertainment Inc.</title>
@@ -11,13 +24,14 @@
 		<script src="js/skel-panels.min.js"></script>
 		<script src="js/skel.min.js"></script>
 		<script src="js/init.js"></script>
+		<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
 		</head> 
 	<body class="homepage">
 		<div id="header-wrapper">
 			<div class="container">
 				<div id="header">
 					<div id="logo">  <!-- 考慮放LOGO -->
-						<h1><a href="#">Colorful Entertainment</a></h1>  
+						<h1><a href="index.php">Colorful Entertainment</a></h1>  
 						<span>Inc.</span>
 					</div>
 					<nav id="nav">
@@ -30,19 +44,48 @@
                             	</ul>
 							</li>
 							<li><a class="link link--index" href="test.php"><span data-letters-l="藝" data-letters-r="人">藝人</span></a></li>
-							<li><a class="link link--index" href="test.php"><span data-letters-l="影" data-letters-r="音">影音</span></a></li>
 							<li><a class="link link--index" href="test.php"><span data-letters-l="購物" data-letters-r="商城">購物商城</span></a></li>
-							<li><a class="link link--index" href="test.php"><span data-letters-l="會員" data-letters-r="專區">會員專區</span></a></li>
+							<li><a class="link link--index" href="test.php"><span data-letters-l="會員" data-letters-r="專區">會員專區</span></a>
+								<ul>
+									<li><a class="link link--secindex" href="test.php"><span data-letters="會員註冊">會員註冊</span></a></li><br>
+    								<li><a class="link link--secindex" href="test.php"><span data-letters="會員登入">會員登入</span></a></li><br>
+    								<li><a class="link link--secindex" href="test.php"><span data-letters="帳號資訊">帳號資訊</span></a></li><br>
+                            	</ul>
+                            </li>
 							<li><a class="link link--index" href="test.php"><span data-letters-l="投" data-letters-r="票">投票</span></a></li>
 						</ul>
 					</nav>
 				</div>
 			</div>
 		</div>
-		<div align="center">
-    		<iframe src="banner.php" height="1000vh" width="2100vw"></iframe>
-		</div>
-
+		<div class="slide-container" id="slide_component">
+		<ul class="slide-content">
+			<li><img src="images/pics01.jpg" width="1280px" height="580px" /></li>
+			<li><img src="images/pics05.jpg" width="1280px" height="580px" /></li>
+			<li><img src="images/pics10.jpg" width="1280px" height="580px" /></li>
+		</ul>
+		<ul class="slide-items">
+			<li class="current-slide-item"></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	<div id="news">
+ 	<h2>最新消息  NEWS</h2>
+    	<ul class="newstyle">
+    	<?php 
+    	if(mysqli_num_rows($result) > 0){   	
+    	    while ($row = mysqli_fetch_array($result, MYSQLI_BOTH)) {
+    	        echo "<li><a class='desc' href='test.php'>". $row['BOARD_DESC'] ."</a></li>";
+    	    }
+    	}
+    	mysqli_close($con);
+         ?>
+        </ul>
+	</div>
 	</body>
 </div>
 </html>
+<script type="text/javascript">
+		$("#slide_component").jSlide();
+</script>
--- test.php
+++ test.php
@@ -1,1 +1,21 @@
 <h1><a href="#">開發中</a></h1>
+<?php 
+//連線至資料庫
+// $hostname  = "148.66.136.151:3306";
+// $username  = "Vicky";
+// $password  = "vicky@851221";
+// $dbname    = "ColorfulENTInc";
+// $usertable = "BOARD";
+// $yourfield = "BOARD_DESC";
+
+// $con = mysqli_connect($hostname,$username, $password,$dbname);
+
+// if(mysqli_connect_errno($con))
+//     die("資料庫無法連線");
+
+//     $sql = "INSERT INTO `BOARD`(`BOARD_SEQNO`, `BOARD_KIND`, `BOARD_ITEM`, `BOARD_DESC`, `BOARD_DATES`, `BOARD_DATEE`) VALUES (\"TEST5678\",\"02\",\"TEST\",\"TEST2\",\"2020-10-18\",\"2020-10-19\")"; // 指定SQL字串
+//     echo "SQL字串: $sql <br/>";
+//     送出UTF8編碼的MySQL指令
+//     mysqli_query($con, 'SET NAMES utf8');
+//     mysqli_query($con, $sql);
+?>
