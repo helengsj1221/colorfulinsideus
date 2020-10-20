bdb1ef721e3e7850c6d18f3243b2970bd56e913b
helen <helen@DESKTOP-IVS9STS>
2020-10-21 01:56:10 +0800

    頁尾 區塊調整

--- index.php
+++ index.php
@@ -1,72 +1,72 @@
 <html>
-	<?php 
-            // 連線至資料庫
-            $hostname  = "148.66.136.151:3306";
-            $username  = "Vicky";
-            $password  = "vicky@851221";
-            $dbname    = "ColorfulENTInc";
-            $today     = date("Ymd");  
-            $con = mysqli_connect($hostname,$username, $password,$dbname);
-            //撈資料
-            $newssql = "SELECT * FROM `BOARD` WHERE `BOARD_DATES`<= ".$today." AND `BOARD_DATEE` >=".$today." ORDER BY `BOARD_DATES` DESC LIMIT 7";
-            mysqli_query($con, 'SET NAMES utf8');
-            $result = mysqli_query($con, $newssql);
-    ?>
+<?php
+    // 連線至資料庫
+    $hostname  = "148.66.136.151:3306";
+    $username  = "Vicky";
+    $password  = "vicky@851221";
+    $dbname    = "ColorfulENTInc";
+    $today     = date("Ymd");
+    $con = mysqli_connect($hostname,$username, $password,$dbname);
+    //撈資料
+    $newssql = "SELECT * FROM `BOARD` WHERE `BOARD_DATES`<= ".$today." AND `BOARD_DATEE` >=".$today." ORDER BY `BOARD_DATES` DESC LIMIT 7";
+    mysqli_query($con, 'SET NAMES utf8');
+    $result = mysqli_query($con, $newssql);
+?>
 <div id="wrapper">
 	<head>
 		<title>Colorful Entertainment Inc.</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<link href="http://fonts.googleapis.com/css?family=Oswald:400,300" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" href="css/style.css" />
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="js/skel-panels.min.js"></script>
 		<script src="js/skel.min.js"></script>
 		<script src="js/init.js"></script>
 		<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+    	<script type="text/javascript" src="js/banner.js"></script>
+    	<script type="text/javascript" src="js/block.js"></script>
 	</head> 
-	
+	<!-- 主要頁面 -->
 	<body class="homepage">
 		<div id="header-wrapper">
 			<div class="container">
 				<div id="header">
+   					<!-- 左上公司名 -->
 					<div id="logo">  <!-- 考慮放LOGO -->
-						<h1><a href="index.php">Colorful Entertainment</a></h1>  
-						<span>Inc.</span>
+						<h1><a href="index.php">Colorful Entertainment</a></h1><span>Inc.</span>
 					</div>
+					<!-- 目錄 -->
 					<nav id="nav">
 						<ul class="drop-down-menu">
-							<li><a class="link link--index" href="index.php"><span data-letters-l="首" data-letters-r="頁">首頁</span></a></li>
-							<li><a class="link link--index" href="test.php"><span data-letters-l="公司" data-letters-r="簡介">公司簡介</span></a>
+							<li><a class="link link-index" href="index.php"><span data-letters-l="首" data-letters-r="頁">首頁</span></a></li>
+							<li><a class="link link-index" href="test.php"><span data-letters-l="公司" data-letters-r="簡介">公司簡介</span></a>
 								<ul>
-    								<li><a class="link link--secindex" href="test.php"><span data-letters="公司沿革">公司沿革</span></a></li><br>
-    								<li><a class="link link--secindex" href="test.php"><span data-letters="聯絡我們">聯絡我們</span></a></li><br>
+    								<li><a class="link link-secindex" href="test.php"><span data-letters="公司沿革">公司沿革</span></a></li><br>
+    								<li><a class="link link-secindex" href="test.php"><span data-letters="聯絡我們">聯絡我們</span></a></li><br>
                             	</ul>
 							</li>
-							<li><a class="link link--index" href="test.php"><span data-letters-l="藝" data-letters-r="人">藝人</span></a></li>
-							<li><a class="link link--index" href="test.php"><span data-letters-l="購物" data-letters-r="商城">購物商城</span></a></li>
-							<li><a class="link link--index" href="test.php"><span data-letters-l="會員" data-letters-r="專區">會員專區</span></a>
+							<li><a class="link link-index" href="test.php"><span data-letters-l="藝" data-letters-r="人">藝人</span></a></li>
+							<li><a class="link link-index" href="test.php"><span data-letters-l="購物" data-letters-r="商城">購物商城</span></a></li>
+							<li><a class="link link-index" href="test.php"><span data-letters-l="會員" data-letters-r="專區">會員專區</span></a>
 								<ul>
-									<li><a class="link link--secindex" href="test.php"><span data-letters="會員註冊">會員註冊</span></a></li><br>
-    								<li><a class="link link--secindex" href="test.php"><span data-letters="會員登入">會員登入</span></a></li><br>
-    								<li><a class="link link--secindex" href="test.php"><span data-letters="帳號資訊">帳號資訊</span></a></li><br>
+									<li><a class="link link-secindex" href="test.php"><span data-letters="會員註冊">會員註冊</span></a></li><br>
+    								<li><a class="link link-secindex" href="test.php"><span data-letters="會員登入">會員登入</span></a></li><br>
+    								<li><a class="link link-secindex" href="test.php"><span data-letters="帳號資訊">帳號資訊</span></a></li><br>
                             	</ul>
                             </li>
-							<li><a class="link link--index" href="test.php"><span data-letters-l="投" data-letters-r="票">投票</span></a></li>
+							<li><a class="link link-index" href="test.php"><span data-letters-l="投" data-letters-r="票">投票</span></a></li>
 						</ul>
 					</nav>
 				</div>
 			</div>
 		</div>
 		</body>
-		
+		<!-- banner -->
 		<body>
     	<div class="slide-container" id="slide_component">
     		<ul class="slide-content">
-    			<li><img src="images/pics01.jpg" width="1280px" height="580px" /></li>
-    			<li><img src="images/pics05.jpg" width="1280px" height="580px" /></li>
-    			<li><img src="images/pics10.jpg" width="1280px" height="580px" /></li>
+    			<li><img src="images/banner01.jpg" width="1280px" height="580px" /></li>
+    			<li><img src="images/banner02.jpg" width="1280px" height="580px" /></li>
+    			<li><img src="images/banner03.jpg" width="1280px" height="580px" /></li>
     		</ul>
     		<ul class="slide-items">
     			<li class="current-slide-item"></li>
@@ -74,13 +74,8 @@
     			<li></li>
     		</ul>
     	</div>
-    	<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-    	<script type="text/javascript" src="js/banner.js"></script>
-    	<script type="text/javascript">
-    		$("#slide_component").jSlide();
-    	</script>
-	</body>
-	
+		</body>
+	<!-- 最新消息 -->
 	<body>
 	<div id="news">
  	<h2>最新消息  NEWS</h2>
@@ -96,5 +91,21 @@
         </ul>
 	</div>
 	</body>
+<!-- 頁尾 -->
+<body>
+<footer id="footer">
+	<div class="container">
+	<h2>聯絡我們</h2><br>
+		<ul class="icons">
+<!-- 			<li><a href="#"><img src="images/FB.png" width="70px" height="70px"/></a></li> -->
+			<li><a href="https://www.instagram.com/colorful.ent_official/"><img src="images/IG.png" width="70px" height="70px"/></a></li>
+			<li><a href="mailto:service@colorful-entertainment-official.com"><img src="images/EMAIL.png" width="70px" height="70px"/></a></li>
+		</ul>
+	</div>
+</footer>
+</body>
 </div>
+
+<script type="text/javascript">$("#slide_component").jSlide();</script>
+<script type="text/javascript">$("#block_slide_component").jbSlide();</script>
 </html>
---
+++ js/block.js
@@ -0,0 +1,97 @@
+;(function() {
+	//切換滾動時間
+	function SlideAlbums(param, context) {
+		var param = param || {};
+		this.ctn = context;
+		this.contentCtn = this.ctn.find(".block-slide-content");
+		this.itemsCtn = this.ctn.find(".block-slide-items");
+		this.slideTime = param.slideTime || 1500;
+		this.stayTime = param.stayTime || 4500;
+		this.init();
+	}
+	$.extend(SlideAlbums.prototype, {
+		initData: function() {
+			this.content = this.contentCtn.children();
+			this.items = this.itemsCtn.children();
+			this.itemsNumber = this.content.size();
+			this.itemWidth = this.content.eq(0).innerWidth();
+			this.contentCtn.css("width", this.itemWidth*this.itemsNumber);
+			this.index = 0;
+		},
+		initEvent: function() {
+			var self = this;
+			this.items.each(function(index, ele) {
+				$(ele).click(function() {
+					if ( !self.isSliding ) {
+						clearTimeout(self.count || null);
+						self.switchTo(index, self.slideEnd);
+					}
+				});
+			});
+		},
+		init: function() {
+			this.initData();
+			this.countTime();
+			this.initEvent();
+		},
+		countTime: function() {
+			var self = this;
+			this.count = setTimeout(function() {
+				self.go(1);
+			}, this.stayTime);
+		},
+		slideEnd: function(index) {
+			this.index = index;
+			this.setCurrentItem(this.index);
+			this.countTime();
+		},
+		switchTo: function(index, callback) {
+			var self = this;
+			this.isSliding = true;
+			this.contentCtn.animate({
+				left: "-"+this.itemWidth*index
+			}, this.slideTime, function() {
+				self.isSliding = false;
+				callback.call(self, index);
+			});
+		},
+		setCurrentItem: function(index) {
+			this.items.removeClass("current-slide-item");
+			this.items.eq(index).addClass("current-slide-item");
+		},
+		go: function(dir) {
+			if ( this.isSliding ) return;
+			if ( dir > 0 ) {
+				if ( this.index == this.itemsNumber-1 ) {
+					this.content.eq(0).css({
+						position: "relative",
+						left: this.itemsNumber*this.itemWidth
+					});
+					this.switchTo(++this.index, function() {
+						this.index = 0;
+						this.content.eq(0).removeAttr("style");
+						this.contentCtn.css("left", "0");
+						this.setCurrentItem(0);
+						this.isSliding = false;
+						this.countTime();
+					});
+					return;
+				}
+				this.switchTo(++this.index, this.slideEnd);
+			}
+		}
+	});
+	
+	$.fn.extend({
+		jbSlide: function(param) {
+			/*
+			 *	slideTime: 1500	滑動執行時間
+			 *	stayTime: 4500	停留時間
+			 * }
+			*/
+			$.each($(this), function() {
+				new SlideAlbums(param, $(this));
+			});
+		}
+	});
+})()
