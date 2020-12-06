c8bbc15a58391f868380c138fd5662061fc71570
helen <helen@DESKTOP-IVS9STS>
2020-12-07 01:26:00 +0800

    資料庫IP更換

--- public/config.php
+++ public/config.php
@@ -1,6 +1,7 @@
 <?php
     // 連線至資料庫
     $hostname  = "148.66.136.151:3306";
+    $hostname  = "127.0.0.1";
     $username  = "Vicky";
     $password  = "vicky@851221";
     $dbname    = "Colorful";
